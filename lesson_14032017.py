fe5d3f48afe85b83b79ae12f42bf16d208fa9289
Yuriy Pustovoytenko <pustovoytenko@ukr.net>
2017-03-16 18:18:25 +0200

    add homework from 14.03.2017

---
+++ lesson_14032017.py
@@ -0,0 +1,26 @@
+# from datetime import datetime, timedelta
+#
+# br = "2017-03-10"
+# br_datetme = datetime.strptime(br,"%Y-%m-%d")
+# br_str = br_datetme.strftime("%m/%d/%Y")
+#
+# import os
+#
+# print(os.listdir())
+
+# from selenium import webdriver
+#
+# import os
+#
+# foxdriver = os.path.join(os.path.dirname(os.path.abspath(__file__)), "geckodriver")
+#
+# print(foxdriver)
+# # os.environ["webdriver.gecko.driver"] = foxdriver
+# #
+# # driver = webdriver.Firefox()
+# #
+# # driver.get("http://toster.ru")
+
+for i in range(1, 5):
+    print(i)
+
